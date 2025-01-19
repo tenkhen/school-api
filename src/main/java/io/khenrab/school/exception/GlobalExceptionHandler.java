01a03bb915f0f7311378fc091f94e0196f5a6224
Khenrab Dorjee Lama <55808802+tenkhen@users.noreply.github.com>
2025-01-19 15:14:38 -0500

    Updated GlobalExceptionHandler

--- src/main/java/io/khenrab/school/exception/GlobalExceptionHandler.java
+++ src/main/java/io/khenrab/school/exception/GlobalExceptionHandler.java
@@ -35,11 +35,17 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErrorDetails> handleRuntimeException(RuntimeException exception, WebRequest webRequest) {
-        ErrorDetails errorDetails = buildErrorDetails(exception, webRequest, HttpStatus.INTERNAL_SERVER_ERROR);
-        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorDetails> handleIllegalArgumentException(IllegalArgumentException exception, WebRequest webRequest) {
+        ErrorDetails errorDetails = buildErrorDetails(exception, webRequest, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
+
+//    @ExceptionHandler(RuntimeException.class)
+//    public ResponseEntity<ErrorDetails> handleRuntimeException(RuntimeException exception, WebRequest webRequest) {
+//        ErrorDetails errorDetails = buildErrorDetails(exception, webRequest, HttpStatus.INTERNAL_SERVER_ERROR);
+//        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
 
     private ErrorDetails buildErrorDetails(Exception exception, WebRequest webRequest, HttpStatus httpStatus) {
         return new ErrorDetails(
